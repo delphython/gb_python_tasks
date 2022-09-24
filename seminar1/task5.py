6159ec8d2b1c951e6f1ee40ab0cf11513bc6086a
delphython <delphython@gmail.com>
2022-09-24 23:02:32 +0300

    add task5

---
+++ seminar1/task5.py
@@ -0,0 +1,24 @@
+'''
+Задание 5. Дано число. Проверить кратно ли оно 5 и 10 или 15, но не 30.
+'''
+
+
+def main():
+    print("Введите целое число")
+    target_number = input()
+
+    if target_number.isdigit():
+        int_target_number = int(target_number)
+        print(
+            (
+                (int_target_number % 5 == 0) &
+                (int_target_number % 10 == 0) |
+                (int_target_number % 15 == 0)
+            ) & int_target_number % 30 != 0
+        )
+    else:
+        print("Введено неверное число")
+
+
+if __name__ == "__main__":
+    main()
