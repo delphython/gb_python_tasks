f6e8d5f81696f6edb237530d7e149cae9b51781c
delphython <delphython@gmail.com>
2022-09-27 12:58:26 +0300

    add task5

---
+++ seminar2/task5.py
@@ -0,0 +1,23 @@
+'''
+Задание 5. Написать программу получающую набор
+произведений чисел от 1 до N.
+'''
+
+
+def main():
+    output_list = []
+    print("Введите натуральное число N")
+    target_number = input()
+
+    if target_number.isdigit():
+        int_target_number = int(target_number)
+        for n in range(1, int_target_number+1):
+            multiply = 1 if n == 1 else n * output_list[n-2] 
+            output_list.append(multiply)
+        print(output_list)
+    else:
+        print("Введено неверное число")
+
+
+if __name__ == "__main__":
+    main()
