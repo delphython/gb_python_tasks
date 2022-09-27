ef20df7b10d90c621875203074a0725219f04374
delphython <delphython@gmail.com>
2022-09-27 12:03:05 +0300

    add task1

---
+++ seminar2/task1.py
@@ -0,0 +1,20 @@
+'''
+Задание 1. Напишите программу, которая принимает на вход
+число N и выдаёт последовательность из N членов..
+'''
+
+
+def main():
+    print("Введите число N")
+    target_number = input()
+
+    if target_number.isdigit():
+        int_target_number = int(target_number)
+        for n in range(int_target_number):
+            print((-3) ** n)
+    else:
+        print("Введено неверное число")
+
+
+if __name__ == "__main__":
+    main()
