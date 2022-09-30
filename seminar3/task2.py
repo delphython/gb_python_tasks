8ba6c021210a1e53e3fdd943dd366868b237a36b
delphython <delphython@gmail.com>
2022-09-30 12:10:33 +0300

    add task2

---
+++ seminar3/task2.py
@@ -0,0 +1,25 @@
+'''
+Задание 2. Задайте список. Напишите программу, которая определит,
+присутствует ли в заданном списке строк некое число.
+'''
+
+
+import math
+
+
+def main():
+    input_list = ["ksd", "lasland", 2.3]
+
+    tmp_str = (
+            "присутствует" if (
+                any(
+                    type(input_value) in {int, float} 
+                    for input_value in input_list
+                )
+            ) else "не присутствует"
+        )
+
+    print(f"В заданном списке строк {tmp_str} некое число")
+
+if __name__ == "__main__":
+    main()
