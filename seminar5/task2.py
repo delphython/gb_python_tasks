bf31b40e27b1620bdac1009c9d5da9214a6a1cd2
delphython <delphython@gmail.com>
2022-10-10 17:58:22 +0300

    make changes

--- seminar5/task2.py
+++ seminar5/task2.py
@@ -5,17 +5,30 @@
 '''
 
 
+def get_increasing_sequence(input_list):
+    output_list = []
+    tmp_list = []
+    
+    input_list_len = len(input_list)
+    
+    for i in range(input_list_len):
+        if i < input_list_len - 1 and input_list[i] <= input_list[i+1]:
+            tmp_list.append(input_list[i])
+        else:
+            tmp_list.append(input_list[i])
+            if len(tmp_list) > 1:
+                output_list.append(tmp_list)
+            tmp_list = []
+    
+    return output_list
+
+
 def main():
-    input_list = [1, 5, 2, 3, 4, 6, 1, 7]
-
-    for i in range(len(input_list)-1):
-        if (input_list[i + 1] > input_list[i]) and ():
-            output_list.append(input_list[i])
-
-
-    output_list = [input_list[i] for i in range(len(input_list)-1) if (input_list[i + 1] > input_list[i])]
+    input_list = [6, 5, 2, 7, 9, 6, 10, 2]
+    output_list = get_increasing_sequence(input_list)
 
     print(output_list)
+
 
 if __name__ == "__main__":
     main()
