8137e9dc8e6cf7645fedbfd58dcf1375994026f4
delphython <delphython@gmail.com>
2022-10-13 16:24:13 +0300

    add task1

---
+++ seminar6/task1.py
@@ -0,0 +1,48 @@
+'''
+Задание 1. Напишите программу вычисления арифметического
+выражения заданного строкой. Используйте операции +,-,/,*
+приоритет операций стандартный.
+'''
+
+
+def calculate(expression):
+    operator_function = {
+        '-': lambda x, y: x - y,
+        '+': lambda x, y: x + y,
+        '*': lambda x, y: x * y,
+        '/': lambda x, y: x / y,
+    }
+
+    numbers = list()
+    operators = list()
+
+    elements = list('(' + expression + ')')
+    
+    while elements:
+        element = elements.pop(0)
+
+        if element.isdecimal():
+            numbers.append(float(element))
+        else:
+            if element == ')':
+                operator = operators.pop()
+                while operators and operator != '(':
+                    x, y = numbers.pop(), numbers.pop()
+                    f = operator_function[operator]
+                    numbers.append(f(x, y))
+
+                    operator = operators.pop()
+            else:
+                operators.append(element)
+
+    return numbers[0]
+
+
+def main():
+    print("Введите арифметическое выражение")
+    arithmetic_expression = input()
+    print(calculate(arithmetic_expression))
+
+
+if __name__ == "__main__":
+    main()
