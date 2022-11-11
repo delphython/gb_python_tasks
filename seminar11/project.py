fcf1b947a3e8b05fea18fb81b4bca1b7ccfbdd92
delphython <delphython@gmail.com>
2022-11-11 16:57:28 +0300

    add project

---
+++ seminar11/project.py
@@ -0,0 +1,52 @@
+"""
+f(x) = -12x^4*sin(cos(x)) - 18x^3+5x^2 + 10x - 30
+Определить корни
+Найти интервалы, на которых функция возрастает
+Найти интервалы, на которых функция убывает
+Построить график
+Вычислить вершину
+Определить промежутки, на котором f > 0
+Определить промежутки, на котором f < 0
+"""
+
+
+def get_equation_roots():
+    pass
+
+
+def get_function_increase_intervals():
+    pass
+
+
+def get_function_decrease_intervals():
+    pass
+
+
+def draw_graph():
+    pass
+
+
+def calculate_top():
+    pass
+
+
+def get_intervals_greater_zero():
+    pass
+
+
+def get_intervals_less_zero():
+    pass
+
+
+def main():
+    print(get_equation_roots())
+    print(get_function_increase_intervals())
+    print(get_function_decrease_intervals())
+    print(draw_graph())
+    print(calculate_top())
+    print(get_intervals_greater_zero())
+    print(get_intervals_less_zero())
+
+
+if __name__ == "__main__":
+    main()
