885e3b415b481bf5052dd845969092668e511531
delphython <delphython@gmail.com>
2022-11-17 12:07:28 +0300

    add many calculations

--- seminar11/project.py
+++ seminar11/project.py
@@ -10,35 +10,71 @@ f(x) = -12x^4*sin(cos(x)) - 18x^3+5x^2 + 10x - 30
 """
 
 
-from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 import numpy as np
-from gekko import GEKKO
+from sympy import Symbol, diff, nsolve, Eq, sin, cos
 
 
-def get_equation_roots():
-    m = GEKKO(remote=False)
-    x = m.Var(1)
-    expression = -12*x**4*m.sin(m.cos(x)) - 18*x**3+5*x**2 + 10*x - 30
+def get_function_derivative(m, expression, value):
+    m.Equation(expression.dt())
+    m.options.IMODE = 4
+    m.solve(disp=False) 
 
-    m.Equation(expression == 0)
-    m.solve(disp=False)
-    solves = [float(x.value[0])]
-    for x.value in range(-22, 22):
-        m.solve(disp=False)
-        solves.append(float(x.value[0]))
 
-    x = np.array(solves)
+def get_equation_roots(x, expression):
+    roots = []
+
+    start = -22
+    stop = 22
+    step = 0.1
+    current = start
+
+    for x0 in np.arange(current, stop + step, step):
+        if x0 < current:
+            continue
+        sol = nsolve(expression, x, x0, verify=False)
+        if sol is None:
+            continue
+        current = sol
+        if (sol not in roots) and (sol >= start and sol <= stop):
+            roots.append(sol)
     
-    return list(np.unique(x.round(decimals=10)))
+    return roots
 
 
-def get_function_increase_intervals():
-    pass
+def get_function_increase_decrease_intervals(x, expression):
+    increase_intervals = []
+    decrease_intervals = []
+    interval = [None, None]
+    start = -22
+    stop = 22
+    step = 0.1
 
-
-def get_function_decrease_intervals():
-    pass
+    for x0 in np.arange(start, stop + step, step):
+        derr = expression.subs(x, x0)
+        if derr > 0:
+            if not interval[0]:
+                interval[0] = [x0, derr]
+                tmp_d = derr
+            elif not interval[1]:
+                if tmp_d < 0:
+                   interval[1] = [x0, tmp_d]
+        elif derr < 0:
+            if not interval[0]:
+                interval[0] = [x0, derr]
+                tmp_d = derr
+            elif not interval[1]:
+                if tmp_d > 0:
+                   interval[1] = [x0, tmp_d]
+        if interval[0] and interval[1]:
+            if interval[0][1] > 0 and interval[1][1] > 0:
+                increase_intervals.append([interval[0][0], interval[1][0]])
+                interval = [None, None]
+            elif interval[0][1] < 0  and interval[1][1] < 0:
+                decrease_intervals.append([interval[0][0], interval[1][0]])
+                interval = [None, None]
+        
+    return increase_intervals, decrease_intervals
 
 
 def draw_graph():
@@ -63,13 +99,15 @@ def get_intervals_less_zero():
 
 
 def main():
-    # print(get_equation_roots())
-    print(get_function_increase_intervals())
-    print(get_function_decrease_intervals())
-    print(draw_graph())
-    print(calculate_top())
-    print(get_intervals_greater_zero())
-    print(get_intervals_less_zero())
+    x = Symbol('x')
+    expression = -12*x**4*sin(cos(x)) - 18*x**3+5*x**2 + 10*x - 30
+    fd = diff(expression)
+    
+    print(get_equation_roots(x, expression))
+    print(get_function_increase_decrease_intervals(x, expression))
+    print(get_function_increase_decrease_intervals(x, fd))
+    # print(calculate_top())
+    draw_graph()
 
 
 if __name__ == "__main__":
